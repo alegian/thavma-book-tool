dca2f3ef89c4fd8017ff5641a5bfe039b342eabd
alegian <alegiannx@gmail.com>
2025-12-26 12:50:48 +0200

    basic homepage

--- components/pages/HomePage.tsx
+++ components/pages/HomePage.tsx
@@ -1,19 +1,26 @@
 "use client";
-import { Button } from "@heroui/react";
-import { Column } from "../util/View";
-import { ReactNode } from "react";
+import { Button, ButtonProps } from "@heroui/react";
+import { Column, View } from "../util/View";
 
 export default function HomePage() {
   return (
-    <Column cn="min-h-dvh items-center justify-center">
-      <Column cn="items-center gap-sm">
-        <MainButton>Import</MainButton>
-        <MainButton>New</MainButton>
+    <Column cn="min-h-dvh items-center justify-center bg-background">
+      <Column cn="items-center gap-lg">
+        <View cn="t-title text-foreground">Thavma Book Tool</View>
+
+        <Column cn="gap-sm">
+          <MainButton>New</MainButton>
+          <MainButton variant="secondary">Import</MainButton>
+        </Column>
       </Column>
     </Column>
   );
 }
 
-function MainButton({ children }: { children: ReactNode }) {
-  return <Button size="lg">{children}</Button>;
+function MainButton({ children, ...rest }: ButtonProps) {
+  return (
+    <Button size="lg" fullWidth {...rest}>
+      {children}
+    </Button>
+  );
 }
