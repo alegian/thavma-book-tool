40334ebc97a42b9e66bd90eb1fc02dfc87697847
alegian <alegiannx@gmail.com>
2025-12-26 22:58:05 +0200

    some basics

--- components/FastSprite.tsx
+++ components/FastSprite.tsx
@@ -1,11 +1,19 @@
 import { Assets } from "pixi.js";
 import { ComponentProps, useMemo } from "react";
 
-export default function FastSprite({
+export function FastSprite({
   texture,
   ...rest
 }: Omit<ComponentProps<"pixiSprite">, "texture"> & { texture: string }) {
   const fastTexture = useMemo(() => Assets.get(texture), [texture]);
   return <pixiSprite texture={fastTexture} {...rest} />;
+}
+
+export function FastTilingSprite({
+  texture,
+  ...rest
+}: Omit<ComponentProps<"pixiTilingSprite">, "texture"> & { texture: string }) {
+  const fastTexture = useMemo(() => Assets.get(texture), [texture]);
+  return <pixiTilingSprite texture={fastTexture} {...rest} />;
 }
 
--- components/pages/GraphPage.tsx
+++ components/pages/GraphPage.tsx
@@ -1,20 +1,22 @@
 "use client";
-import { Application, extend } from "@pixi/react";
-import { Assets, Container, Graphics, Sprite, Texture } from "pixi.js";
+import { Application, extend, useApplication } from "@pixi/react";
+import { Assets, Container, Graphics, Sprite, TilingSprite } from "pixi.js";
 import { useEffect, useRef, useState } from "react";
 import { Column, View } from "../util/View";
 import { textures } from "@/services/textures";
-import FastSprite from "../FastSprite";
+import { FastSprite, FastTilingSprite } from "../FastSprite";
 
 extend({
   Container,
   Graphics,
   Sprite,
+  TilingSprite,
 });
 
+const SCALE = 2;
+
 export default function GraphPage() {
-  const spriteRef = useRef(null);
-  const parentRef = useRef(null);
+  const parentRef = useRef<HTMLDivElement>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -25,12 +27,81 @@ export default function GraphPage() {
 
   if (loading) return null;
   return (
-    <Column className="h-dvh items-center justify-center bg-white">
-      <View cn="aspect-video h-11/12" ref={parentRef}>
-        <Application resizeTo={parentRef}>
-          <FastSprite ref={spriteRef} texture={textures.TAB_BG} x={0} y={0} />
+    <Column className="h-dvh justify-center bg-white p-3">
+      <View cn="grow" ref={parentRef}>
+        <Application resizeTo={parentRef} antialias={false}>
+          <Tab />
+          <Frame />
         </Application>
       </View>
     </Column>
+  );
+}
+
+function Tab() {
+  const { app } = useApplication();
+
+  return (
+    <pixiContainer
+      mask={new Graphics()
+        .rect(16, 16, app.screen.width - 32, app.screen.height - 32)
+        .fill(0x000000)}
+    >
+      <Background />
+      <FastSprite texture={textures.NODE} x={128} y={128} scale={SCALE} />
+    </pixiContainer>
+  );
+}
+
+function Background() {
+  const { app } = useApplication();
+
+  return (
+    <FastTilingSprite
+      texture={textures.TAB_BG}
+      anchor={0.5}
+      x={app.screen.width / 2}
+      y={app.screen.height / 2}
+      width={2 * 3840}
+      height={2 * 2160}
+      scale={SCALE}
+    />
+  );
+}
+
+function Frame() {
+  const { app } = useApplication();
+
+  return (
+    <>
+      <FastSprite
+        texture={textures.FRAME_CORNER}
+        anchor={{ x: 0, y: 0 }}
+        x={0}
+        y={0}
+        scale={SCALE}
+      />
+      <FastSprite
+        texture={textures.FRAME_CORNER}
+        anchor={{ x: 0, y: 1 }}
+        x={0}
+        y={app.renderer.screen.height}
+        scale={SCALE}
+      />
+      <FastSprite
+        texture={textures.FRAME_CORNER}
+        anchor={{ x: 1, y: 1 }}
+        x={app.screen.width}
+        y={app.screen.height}
+        scale={SCALE}
+      />
+      <FastSprite
+        texture={textures.FRAME_CORNER}
+        anchor={{ x: 1, y: 0 }}
+        x={app.screen.width}
+        y={0}
+        scale={SCALE}
+      />
+    </>
   );
 }
