1600adbccacf40f24380f23ca8dd12d8f426793c
alegian <alegiannx@gmail.com>
2025-12-27 15:04:40 +0200

    switch to threejs

--- components/pages/GraphPage.tsx
+++ components/pages/GraphPage.tsx
@@ -1,107 +1,71 @@
 "use client";
-import { Application, extend, useApplication } from "@pixi/react";
-import { Assets, Container, Graphics, Sprite, TilingSprite } from "pixi.js";
-import { useEffect, useRef, useState } from "react";
-import { Column, View } from "../util/View";
-import { textures } from "@/services/textures";
-import { FastSprite, FastTilingSprite } from "../FastSprite";
+import { useEffect } from "react";
+import { View } from "../util/View";
+import * as THREE from "three";
 
-extend({
-  Container,
-  Graphics,
-  Sprite,
-  TilingSprite,
-});
-
-const SCALE = 2;
-
+const threeCanvas = "three-canvas";
 export default function GraphPage() {
-  const parentRef = useRef<HTMLDivElement>(null);
-  const [loading, setLoading] = useState(true);
-
   useEffect(() => {
-    Promise.all(
-      Object.values(textures).map((texture) => Assets.load(texture)),
-    ).then(() => setLoading(false));
+    draw();
   }, []);
 
-  if (loading) return null;
   return (
-    <Column className="h-dvh justify-center bg-white p-3">
-      <View cn="grow" ref={parentRef}>
-        <Application resizeTo={parentRef} antialias={false}>
-          <Tab />
-          <Frame />
-        </Application>
-      </View>
-    </Column>
+    <View className="h-dvh bg-white p-3">
+      <canvas className="block size-full" id={threeCanvas} />
+    </View>
   );
 }
 
-function Tab() {
-  const { app } = useApplication();
+function draw() {
+  const canvas = document.getElementById(threeCanvas);
+  if (!canvas) return;
 
-  return (
-    <pixiContainer
-      mask={new Graphics()
-        .rect(16, 16, app.screen.width - 32, app.screen.height - 32)
-        .fill(0x000000)}
-    >
-      <Background />
-      <FastSprite texture={textures.NODE} x={128} y={128} scale={SCALE} />
-    </pixiContainer>
+  const scene = new THREE.Scene();
+  const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000,
   );
+
+  const renderer = new THREE.WebGLRenderer({ canvas });
+  renderer.setAnimationLoop(animate);
+
+  const geometry = new THREE.PlaneGeometry(1, 1);
+  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  const plane = new THREE.Mesh(geometry, material);
+  scene.add(plane);
+
+  camera.position.z = 5;
+
+  function animate() {
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement;
+      camera.aspect = canvas.clientWidth / canvas.clientHeight;
+      camera.updateProjectionMatrix();
+    }
+
+    renderer.render(scene, camera);
+  }
 }
 
-function Background() {
-  const { app } = useApplication();
+function resizeRendererToDisplaySize(
+  renderer: THREE.WebGLRenderer,
+  maxPixelCount = 3840 * 2160,
+) {
+  const canvas = renderer.domElement;
+  const pixelRatio = window.devicePixelRatio;
+  let width = Math.floor(canvas.clientWidth * pixelRatio);
+  let height = Math.floor(canvas.clientHeight * pixelRatio);
+  const pixelCount = width * height;
+  const renderScale =
+    pixelCount > maxPixelCount ? Math.sqrt(maxPixelCount / pixelCount) : 1;
+  width = Math.floor(width * renderScale);
+  height = Math.floor(height * renderScale);
 
-  return (
-    <FastTilingSprite
-      texture={textures.TAB_BG}
-      anchor={0.5}
-      x={app.screen.width / 2}
-      y={app.screen.height / 2}
-      width={2 * 3840}
-      height={2 * 2160}
-      scale={SCALE}
-    />
-  );
-}
-
-function Frame() {
-  const { app } = useApplication();
-
-  return (
-    <>
-      <FastSprite
-        texture={textures.FRAME_CORNER}
-        anchor={{ x: 0, y: 0 }}
-        x={0}
-        y={0}
-        scale={SCALE}
-      />
-      <FastSprite
-        texture={textures.FRAME_CORNER}
-        anchor={{ x: 0, y: 1 }}
-        x={0}
-        y={app.renderer.screen.height}
-        scale={SCALE}
-      />
-      <FastSprite
-        texture={textures.FRAME_CORNER}
-        anchor={{ x: 1, y: 1 }}
-        x={app.screen.width}
-        y={app.screen.height}
-        scale={SCALE}
-      />
-      <FastSprite
-        texture={textures.FRAME_CORNER}
-        anchor={{ x: 1, y: 0 }}
-        x={app.screen.width}
-        y={0}
-        scale={SCALE}
-      />
-    </>
-  );
+  const needResize = canvas.width !== width || canvas.height !== height;
+  if (needResize) {
+    renderer.setSize(width, height, false);
+  }
+  return needResize;
 }
